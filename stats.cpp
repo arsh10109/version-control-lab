613b708592056e116820b92a23f68fcc6a22bb4b
Arsh <Arsh2thousand2@gmail.com>
2020-10-05 13:45:56 +0400

    initial commit

---
+++ stats.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+  float x,y,z;
+  cout<<"Enter two numbers: ";
+  cin>>x>>y;
+  cout<<"The sum of "<<x<<" and "<<y<<" is "<<x+y<<endl;
+  cout<<"The difference between "<<x<<" and "<<y<<" is "<<x-y<<endl;
+  cout<<"The product of "<<x<<" and "<<y<<" is "<<x*y<<endl;
+  cout<<"The distance between "<<x<<" and "<<y<<" is "<<abs(x-y)<<endl;
+  cout<<"The mean average of "<<x<<" and "<<y<<" is "<<(x+y)/2<<endl;
+  return 0;
+}
+  
+  
+  
